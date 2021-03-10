346f1b31733650c209cdaa0cf6f7cbfc317074c4
vladislava8sv <57909332+vladislava8sv@users.noreply.github.com>
2021-03-10 14:18:16 +0300

    fead: add solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  var result = [].concat(...matrix);
-}
+  if(typeof matrix != "undefined" && matrix != null && matrix.length > 0){//стандартная проверка на пустую матрицу, нужны все условия тк "Cannot read property 'length' of undefined"
+  var result = [];
+  matrix.forEach((array, i) => { //array-Текущий обрабатываемый элемент в массиве. i-Индекс текущего обрабатываемого элемента в массиве.
+    if (i % 2 == 0) { //индексация массива начинается с нуля, здесть array-строка массива
+      result = result.concat(array)}//соединяет result с array
+    else { 
+      result = result.concat(array.reverse());}//reverse переставляет элементы матрицы в обратном порядке
+  })
+  return result;}
+  else {return ([]);}
+  }
